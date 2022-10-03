a058a2bc4d15d32c217fc64d3288c6c5c05b54e1
Đào Văn Hưng <112063056+GentMaker@users.noreply.github.com>
2022-10-03 08:57:28 -0700

    Bai Tap Lms

---
+++ BaiTap2_Lms/src/ra/dev/test.java
@@ -0,0 +1,11 @@
+package ra.dev;
+
+public class test {
+    public static void main(String[] args) {
+        Point2D test1 = new Point2D(3,4);
+        System.out.println(test1);
+        System.out.println(" ");
+        Point3D test2 = new Point3D(3,3,5);
+        System.out.println(test2);
+    }
+}
